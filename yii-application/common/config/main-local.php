66b08c7be68aa8a1ddf7992cddf449e6e386d738
Ubuntu <ubuntu@ip-172-31-39-183.us-east-2.compute.internal>
2020-04-07 01:09:44 +0000

    add pass phpmyadmin

--- yii-application/common/config/main-local.php
+++ yii-application/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=127.0.0.1;dbname=f2quizz',
             'username' => 'root',
-            'password' => '',
+            'password' => 'f2quizz123',
             'charset' => 'utf8',
         ],
         'mailer' => [
